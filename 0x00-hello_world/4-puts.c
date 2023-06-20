12af26ae4eaa97858e2c75804b21dd4fed902ca5
Fagr Ahmed <fagr748@gmail.com>
2023-06-19 19:01:48 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "write programm using c to print 
+ *                (Programming is like building a multilingual puzzl).
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
