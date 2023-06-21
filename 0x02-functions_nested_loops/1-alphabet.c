9759fb84f81b59232ebf02c760a0e0a903aa4709
Fagr Ahmed <fagr748@gmail.com>
2023-06-20 17:50:31 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lower case.
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
