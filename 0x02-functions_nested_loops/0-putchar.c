367a4799d10d724b5a532c9b7121a747b98e892d
Fagr Ahmed <fagr748@gmail.com>
2023-06-20 18:20:17 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include "holberton.h"
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry Point.
  *
- * Return: On success 1.
+ * Return: Always 0.
  */
-int _putchar(char c)
+
+int main(void):
 {
-	return (write(1, &c, 1));
+	char str[] = "_putchar";
+	int ch;
+
+	for(ch=0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
 }
