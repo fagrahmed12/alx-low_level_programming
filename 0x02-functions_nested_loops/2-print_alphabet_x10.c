7bcb1a732aea178d1c947414cebaac592b282a42
Fagr Ahmed <fagr748@gmail.com>
2023-06-20 19:22:47 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,17 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	char i;
+	int i;
+	char c;
 
-	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
-
-	putchar('\n');
-
-	return (0);
+	for (i = 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
