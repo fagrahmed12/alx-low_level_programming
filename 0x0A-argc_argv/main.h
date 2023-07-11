77c7781f0efcbe210e28bfb83db488e97b89734a
Fagr Ahmed <fagr748@gmail.com>
2023-07-11 14:03:11 -0700

    a

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,18 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
- */
-
+#ifndef __MAIN__
+#define __MAIN__
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+int is_integer(const char *str);
+int get_coin_number(int amount, int nb_coins);
 #endif
