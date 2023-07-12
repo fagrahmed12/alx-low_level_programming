ccb978772449561c7ee9707b02abfb8d4943688d
Fagr Ahmed <fagr748@gmail.com>
2023-07-12 09:15:55 -0700

    a

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,8 +3,8 @@
 
 /**
  * free_grid - Frees a 2-dimensional array of integers.
- * @grid: The 2-dimensional array of integers to be freed.
- * @height: The height of grid.
+ * @grid:The 2-dimensional array of integers to be freed.
+ * @height:The height of grid.
  */
 void free_grid(int **grid, int height)
 {
