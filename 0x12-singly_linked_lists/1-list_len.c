2d060706fe19e88ba1de7cd365b2bcd347bd6bd0
Fagr Ahmed <fagr748@gmail.com>
2023-07-28 14:30:22 -0700

    a

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t cont = 0;
 
 	while (h)
 	{
-		elements++;
+		cont++;
 		h = h->next;
 	}
 
-	return (elements);
+	return (cont);
 }
