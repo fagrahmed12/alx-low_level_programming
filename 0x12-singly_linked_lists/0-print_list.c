6795bc0bc86d38257bb3c96c32f49a265bc4a583
Fagr Elaskary <48035751+fagrahmed12@users.noreply.github.com>
2023-07-29 06:35:22 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t cont = 0;
+	size_t s = 0;
 
 	while (h)
-	{
-		if (h->str)
-			printf("[%u] %s\n", h->len, h->str);
-		else
-			printf("[0] (nil)\n");
-		cont++;
-		h = h->next;
-	}
+	 {
+ 		if (!h->str)
+			 printf("[0] (nil)\n");
+ 		else
+ 			printf("[%u] %s\n", h->len, h->str);
+			 h = h->next;
+			 s++;
+ 	}
 
-	return (cont);
+ 	return (s);
+}
+
+
 }
