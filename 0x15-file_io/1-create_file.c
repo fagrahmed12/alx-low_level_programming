714b532304e7df1398051be1d360f296cff5b0f0
Automated <actions@users.noreply.github.com>
2023-08-11 16:44:22 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,8 @@
 
 /**
  * create_file - creates a file.
+ * @filename: name of the file to create.
+ * @text_content: is a NULL terminated string to write to the file.
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
