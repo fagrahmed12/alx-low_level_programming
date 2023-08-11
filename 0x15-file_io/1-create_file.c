9822356b33904f39b9bd8661955457cdf4ce5ae8
Automated <actions@users.noreply.github.com>
2023-08-11 16:08:21 -0700

    a

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, length = 0;
+
+	if (!filename)
+		return (-1);
+	file_descriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file_descriptor < 0)
+		return (-1);
+
+	while (text_content && text_content[length])
+		length++;
+	if (write(file_descriptor, text_content, length) < 0)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
+	close(file_descriptor);
+	return (1);
+}
