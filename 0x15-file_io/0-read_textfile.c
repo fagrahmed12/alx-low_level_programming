85b338480e467c60eca10649f40dd2d3eca4a004
Automated <actions@users.noreply.github.com>
2023-08-11 19:21:41 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,45 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX std-output.
- * @filename: the file to open.
+ * read_textfile - reads text file, prints it to the POSIX std-output.
+ * @filename: to open the file.
  * @letters: number of letters it should read and print.
  * Return: number of letters it could read and print, or 0 if:
- * - filename is NULL.
- * - the file can not be opened or read.
+ * - filename  NULL.
+ * - file can not be opened or read.
  * - write fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor = -1;
+	int file_des = -1;
 	ssize_t output = 0;
 	char *buffer;
 
 	if (!filename)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor < 0)
+	file_des = open(filename, O_RDONLY);
+	if (file_des < 0)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 	{
-		close(file_descriptor);
+		close(file_des);
 		return (0);
 	}
 
-	output = read(file_descriptor, buffer, letters);
+	output = read(file_des, buffer, letters);
 	if (output < 0)
 	{
 		free(buffer);
-		close(file_descriptor);
+		close(file_de);
 		return (0);
 	}
 
 	output = write(STDOUT_FILENO, buffer, output);
 	free(buffer);
-	close(file_descriptor);
+	close(file_des);
 	if (output < 0)
 		return (0);
 	return (output);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,32 +2,32 @@
 
 /**
  * append_text_to_file - appends text at the end of a file.
- * @filename: name of the file to append the text.
- * @text_content: the NULL terminated string to add at the end of the file.
+ * @filename: name of file to append text.
+ * @text_content: NULL terminated string to add at the end of the file.
  * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descriptor, length = 0;
+	int file_des, length = 0;
 
 	if (!filename)
 		return (-1);
-	file_descriptor = open(filename, O_RDWR | O_APPEND);
-	if (file_descriptor < 0)
+	file_des = open(filename, O_RDWR | O_APPEND);
+	if (file_des < 0)
 		return (-1);
 	if (!text_content)
 	{
-		close(file_descriptor);
+		close(file_des);
 		return (1);
 	}
 
 	while (text_content[length])
 		length++;
-	if (write(file_descriptor, text_content, length) < 0)
+	if (write(file_des, text_content, length) < 0)
 	{
-		close(file_descriptor);
+		close(file_des);
 		return (-1);
 	}
-	close(file_descriptor);
+	close(file_des);
 	return (1);
 }
